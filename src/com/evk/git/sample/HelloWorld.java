6b1d4667118577944dfb175c1ae6506a77b036ec
VIJAYAKUMAR <VIJAYAKUMAR@192.168.0.12>
2016-04-13 20:51:42 -0400

    converted the project facet to java. Added HelloWorld.java

---
+++ src/com/evk/git/sample/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.evk.git.sample;
+
+/**
+ * @author VIJAYAKUMAR
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!...");
+	}
+
+}
