54e382ec2e15886e89dccb69d23dd419cbefb139
EC2 Default User <ec2-user@ip-172-31-20-27.us-west-2.compute.internal>
2016-12-27 18:53:05 +0000

    new changesa by vijay

--- src/com/evk/git/sample/HelloWorld.java
+++ src/com/evk/git/sample/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello, World!...");
+		System.out.println("Hello, World!... vinod");
 	}
 
 }
